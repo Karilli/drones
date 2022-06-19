eb954ea6fd291773a8ec8ca09b96765e8a915dde
Karilli <87448958+Karilli@users.noreply.github.com>
2022-06-19 21:43:28 +0200

    init

---
+++ DroneProject/src/DraftDetection.py
@@ -0,0 +1,7 @@
+from typing import Optional, Tuple
+
+
+class DraftDetector:
+    def find(self, image) -> Optional[Tuple[int, int, int, int]]:
+        """Returns None if there are no draft marks in image, otherwise returns x, y, width, height of the marks."""
+        pass
---
+++ DroneProject/src/Kernel.py
@@ -0,0 +1,7 @@
+def main():
+    """Implements kernel of the drone. Schedules procedure calls."""
+    pass
+
+
+if __name__ == '__main__':
+    main()
