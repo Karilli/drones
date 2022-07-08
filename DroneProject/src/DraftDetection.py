d83efa823b0f98215d36af59384b19c4a7eb029f
Karilli <87448958+Karilli@users.noreply.github.com>
2022-07-08 23:43:40 +0200

    Delete DraftDetection.py

--- DroneProject/src/DraftDetection.py
+++
@@ -1,7 +0,0 @@
-from typing import Optional, Tuple
-
-
-class DraftDetector:
-    def find(self, image) -> Optional[Tuple[int, int, int, int]]:
-        """Returns None if there are no draft marks in image, otherwise returns x, y, width, height of the marks."""
-        pass
